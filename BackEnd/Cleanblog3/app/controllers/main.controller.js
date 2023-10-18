f6609425bb51e06e1e52a3446e96e44b134b1400
A7med7mada <142319059+A7med7mada@users.noreply.github.com>
2023-10-18 09:09:26 +0300

    uploud tasks

---
+++ BackEnd/Cleanblog3/app/controllers/main.controller.js
@@ -0,0 +1,31 @@
+function index(req , res) {
+    // res.send('<h1>Hello from main controller </h1>')
+    const students = ['Ahmed Mazhar' , 'Ahmed Salman' , 'Ahmed Nagy']
+    res.render('pages/index.ejs' , 
+    {title : "Home Page" , students});
+ }
+ 
+ function about(req , res) {
+     res.render('pages/about.ejs' , 
+     {title : "About Page"});
+ }
+ 
+ function contact(req , res) {
+     res.render('pages/contact.ejs' , {
+         title : "Contact Page"
+     });
+ }
+
+
+ function post(req , res) {
+    res.render('pages/post.ejs' , {
+        title : "post Page"
+    });
+}
+ 
+ module.exports = {
+     index,
+     about ,
+     contact,
+     post,
+ }
